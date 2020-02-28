869e45d967c20b709b2e5f08e39e374ff54201b6
matt <xxmaker809xx@gmail.com>
2020-02-28 11:03:35 -0500

    updated images

--- src/yourdata.js
+++ src/yourdata.js
@@ -14,7 +14,7 @@ export default
         contactEmail:'Sadesanol@gmail.com',
         // Add Your About Text Here
         abouttext: "I'm Matt and for a long time I wondered and stressed fairly about what I wanted to do with the rest of my life, That is until i found the UCF coding bootcamp! Little did I know that I would fall in love so easily with the process of coding and the joy of building something from scratch with my ideas in mind!",
-        aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        aboutImage:'https://scontent.ftpa1-1.fna.fbcdn.net/v/t1.0-9/13600304_10209815109198940_8552446720569996691_n.jpg?_nc_cat=108&_nc_sid=e007fa&_nc_ohc=vXoDnSQdnpMAX9_LS0R&_nc_ht=scontent.ftpa1-1.fna&oh=2ef138ccee804762f555aa1656b77074&oe=5EB968F0',
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
        // Change Projects Here 
@@ -24,7 +24,7 @@ export default
             title:'Strain vein (1st iteration)', //Project Title - Add Your Project Title Here
              service:'Website Development', // Add Your Service Type Here
             //Project Image - Add Your Project Image Here
-             imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+             imageSrc:"https://images-na.ssl-images-amazon.com/images/I/81Ht89PpKTL._AC_SX425_.jpg",
              //Project URL - Add Your Project Url Here
              url:'https://github.com/aoacows/Project-1-Mattp-MattV-Noel-Kyle'
             },
@@ -32,7 +32,7 @@ export default
                 id: 2,
                 title: 'The Lounge (1st iteration)',
                 service: 'Website Development',
-                imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                imageSrc: "https://cdn.dribbble.com/users/1146271/screenshots/3020700/csgo_gambling_site.png",
                 url: 'https://github.com/leeumt8/Project-2-Lounge'
             },
             { 
